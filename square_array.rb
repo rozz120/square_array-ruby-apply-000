7b9734fad3627f78cb3526522224ca9d572a6125
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-11-13 10:52:04 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,8 @@ def square_array(array)
   squaredArray =[]
   
   array.each { |number|  number*number
-  squaredArray.push number}
+  squaredArray.push number
+    return squaredArray
+  }
   
 end
