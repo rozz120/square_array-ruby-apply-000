add12992d9639f0700a5d67df459bd20a9490f48
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-11-13 11:08:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,6 @@
 def square_array(array)
   
-  squaredArray =[]
-  
-  array.each do  |number| 
-  squaredNumber = number*number
-  squaredArray.push(squaredNumber)
-  
-  end
-return squaredArray
-end
+
 
 
 
